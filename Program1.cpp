42477258567a1c8ca000c4529be6307ad09a8b72
Bharat venkata siva <118821851+Bharat613@users.noreply.github.com>
2023-10-05 21:08:13 +0530

    Update Program1.cpp

--- Program1.cpp
+++ Program1.cpp
@@ -1,3 +1,12 @@
+/*
+C++ is a powerful and versatile programming language widely used in the software development industry. 
+This course is designed to help you grasp the essential elements of C++ programming, from the basics to more advanced topics.
+Whether you are a beginner or have some programming experience, this course will provide you with a solid foundation in C++.
+In addition to learning C++, we will also delve into the world of data structures and algorithms.
+Data structures are crucial for organizing and storing data efficiently, while algorithms provide the tools and techniques
+to process and manipulate that data effectively. By understanding these fundamental concepts, you will be equipped to 
+solve complex problems and optimize your code.
+*/
 #include<bits/stdc++.h>
 using namespace std;
 int main(){ 
