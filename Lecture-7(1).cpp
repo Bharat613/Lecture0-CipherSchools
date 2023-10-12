516a0b38c7a466ce7875ae11aba189574febcb69
Bharat venkata siva <118821851+Bharat613@users.noreply.github.com>
2023-10-12 23:51:56 +0530

    Create Lecture-7(1).cpp
    do while

---
+++ Lecture-7(1).cpp
@@ -0,0 +1,22 @@
+#incliude<iostream>
+using namespace std;
+int main(){ 
+  int password;
+do{ 
+cin>>password;
+}while(password<999999);
+int count = 0;
+  while(1){ 
+  cout<<"chocolates!"<<endl;
+    if(count >100){ 
+    break;
+    }
+  }
+
+  for(int i=0; ;++i){ 
+    cout<<"Chocolates!";
+  if(i<100){ 
+  break;
+  }
+  }
+}
