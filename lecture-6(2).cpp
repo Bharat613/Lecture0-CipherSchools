4a6db009474015e69154b7d77a50c050c39ea15d
Bharat venkata siva <118821851+Bharat613@users.noreply.github.com>
2023-10-12 23:33:46 +0530

    Create lecture-6(2).cpp
    Loops and increment and decrement of values

---
+++ lecture-6(2).cpp
@@ -0,0 +1,18 @@
+#niclude<iostream>
+uisng namespace std;
+int main(){ 
+int i,j,k;
+i = 0;
+j = 0;
+k = 0;
+cout<<i<<" "<<j<<endl;
+  j = i++;
+  cout<<i<<" "<<j<<endl;
+  k = ++i;
+  cout<<i<<" "<<j<<endl;
+
+  int i=0;
+  for(i = 0;i<101;++i){ 
+    cout<<i<<" ";
+  }
+}
