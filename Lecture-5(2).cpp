32c34dd1c6a3e5639b38782c11ad42bbc33b071d
Bharat venkata siva <118821851+Bharat613@users.noreply.github.com>
2023-10-12 21:21:31 +0530

    Create Lecture-5(2).cpp
    arrays

---
+++ Lecture-5(2).cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int arr[5];
+  int i;
+  cot
+  for(i=0;i<5;++i){ 
+  cin>>arr[i];
+  }
+  
+  for(int j=4;j>=0;--j){ 
+  cout<<arr[i]<<endl;
+  } 
+}
